20e64d867146399c10047e5ad7ac2c8ebf2129d7
codehomie1 <mogpt2x@gmail.com>
2026-02-15 15:31:04 -0800

    Update schema to include comments table with foreign key to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_12_012240) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_15_232333) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "link_id"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_comments_on_link_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
@@ -47,4 +57,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_12_012240) do
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
     t.index ["voter_type", "voter_id"], name: "index_votes_on_voter"
   end
+
+  add_foreign_key "comments", "users"
 end
