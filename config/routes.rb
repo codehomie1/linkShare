f7df0966f628a15a46c65b105a39b4c7ed9dece9
codehomie1 <mogpt2x@gmail.com>
2026-02-15 20:26:39 -0800

    Refactor routes to nest comments under links and remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users
   root "links#index"
   resources :links do
+    resources :comments
     member do
       put "like" => "links#like"
-    end
-
-    member do
       put "dislike" => "links#dislike"
     end
   end
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
