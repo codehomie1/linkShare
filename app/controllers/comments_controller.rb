fa5c3c69e3a48444e3eae1c416c149cc7155d6e9
codehomie1 <mogpt2x@gmail.com>
2026-02-16 11:06:39 -0800

    Refactor CommentsController to associate comments with links and update routes for nested resources

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,29 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
+  before_action :set_link, only: %i[ new create ]
 
-  # GET /comments or /comments.json
+  # GET links/:link_id/comments or /comments.json
   def index
     @comments = Comment.all
   end
 
-  # GET /comments/1 or /comments/1.json
+  # GET links/:link_id/comments/1 or /comments/1.json
   def show
   end
 
-  # GET /comments/new
+  # GET links/:link_id/comments/new
   def new
-    @comment = Comment.new
+    @comment = @link.comments.build
   end
 
-  # GET /comments/1/edit
+  # GET links/:link_id/comments/1/edit
   def edit
   end
 
-  # POST /comments or /comments.json
+  # POST links/:link_id/comments or /comments.json
   def create
-    @link = Link.find(params[:link_id])
     @comment = @link.comments.new(comment_params)
-    @comment_user = @comment.user
+    @comment.user = current_user
 
     respond_to do |format|
       if @comment.save
@@ -36,11 +36,11 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1 or /comments/1.json
+  # PATCH/PUT links/:link_id/comments/1 or /comments/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: "Comment was successfully updated.", status: :see_other }
+        format.html { redirect_to @comment.link, notice: "Comment was successfully updated.", status: :see_other }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,17 +49,21 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1 or /comments/1.json
+  # DELETE /links/:link_id/comments/:id or /comments/1.json
   def destroy
     @comment.destroy!
 
     respond_to do |format|
-      format.html { redirect_to comments_path, notice: "Comment was successfully destroyed.", status: :see_other }
+      format.html { redirect_to link_comments_path(@comment.link), notice: "Comment was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
 
   private
+    def set_link
+      @link = Link.find(params[:link_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params.expect(:id))
@@ -67,6 +71,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.expect(comment: [ :body, :link_id, :user_id ])
+      params.require(:comment).permit(:body)
     end
 end
